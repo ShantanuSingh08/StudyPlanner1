11c3a6126de90996b3b94a6b9a129683f0078bdb
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 03:50:52 +0530

    Update FocusTime.js

--- models/FocusTime.js
+++ models/FocusTime.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose');
 
 const focusTimeSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  dailyFocusTime: [{ date: Date, timeSpent: Number }], // Array to track last 7 days' focus time
-  weeklyFocusTime: [{ weekStart: Date, timeSpent: Number }], // Array for weekly focus time, capped at 4 entries
-  monthlyFocusTime: [{ month: String, timeSpent: Number }] // Array to track monthly focus time
+  dailyFocusTime: [{ 
+    date: Date, 
+    timeSpent: Number 
+  }], // Array to track last 7 days' focus time
+  
+  weeklyFocusTime: [{ 
+    weekStart: Date, 
+    timeSpent: Number 
+  }], // Array for weekly focus time, capped at 4 entries
+  
+  monthlyFocusTime: [{ 
+    month: String, 
+    timeSpent: Number 
+  }] // Array to track monthly focus time
 });
 
 const FocusTime = mongoose.model('FocusTime', focusTimeSchema);
