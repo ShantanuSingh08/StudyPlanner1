785c185b77d271fe72d0dd0a47252abfd877e822
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 03:47:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const focusRoutes = require('./routes/focusRoutes'); // Import focus time routes
 require('dotenv').config();
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/users', userRoutes);
+app.use('/api/focus', focusRoutes); // Add route for focus time
 
 // Start the server
 app.listen(PORT, () => {
