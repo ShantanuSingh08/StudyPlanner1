e8459414aefda18070a79f369582aa2600313361
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 17:51:14 +0530

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -3,13 +3,32 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  class: { type: String },
-  school: { type: String },
-  dob: { type: Date },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  ip: { type: String },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensure email is unique
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  classOrCourse: {
+    type: String,
+    required: true,
+  },
+  school: {
+    type: String,
+    required: true,
+  }
 });
 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('User', userSchema);
