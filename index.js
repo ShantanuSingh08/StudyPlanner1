24f450d0fea26d39449a150aef77aba15d35d96a
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 22:43:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
+const focusTimeRoutes = require('./routes/focusTimeRoutes'); // Import focus time routes
 require('dotenv').config();
 
 const app = express();
@@ -20,6 +21,9 @@ app.use(express.json());
 
 // Use user routes
 app.use('/api', userRoutes); // All user routes will be prefixed with /api
+
+// Use focus time routes
+app.use('/api', focusTimeRoutes); // All focus time routes will be prefixed with /api
 
 // Start server
 app.listen(PORT, () => {
