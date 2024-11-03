4077e8f353530373eb60a2be588fc207c89c70e5
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 17:05:27 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,36 +1,12 @@
+// routes/userRoutes.js
 const express = require('express');
+const { registerUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/Users'); // Make sure the path to your User model is correct
-require('dotenv').config();
 
 // Registration endpoint
-router.post('/register', async (req, res) => {
-  const { email, password, name, dob, classOrCourse, school } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ email, password: hashedPassword, name, dob, classOrCourse, school });
-    await user.save();
-    res.status(201).json({ message: 'Registration successful' });
-  } catch (error) {
-    res.status(400).json({ message: 'Registration failed' });
-  }
-});
+router.post('/register', registerUser);
 
 // Login endpoint
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ message: 'Login successful', token });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+router.post('/login', loginUser);
 
 module.exports = router;
