c54865137632c60933eb29b644b7af941151a124
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 14:47:06 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,24 @@
+// middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authMiddleware = (req, res, next) => {
+  // Get token from the request header
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization denied' });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId; // Attach the userId to the request
+    next(); // Proceed to the next middleware/route handler
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
 
