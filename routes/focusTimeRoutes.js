b738c493adabdd1d69f6ae5e8ea53a0f29c22cf4
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-04 00:18:03 +0530

    Update focusTimeRoutes.js

--- routes/focusTimeRoutes.js
+++ routes/focusTimeRoutes.js
@@ -1,14 +1,14 @@
 // focusTimeRoutes.js
 
 const express = require('express');
-const { saveFocusTime, getFocusTimes } = require('../controllers/focusTimeController');
-
 const router = express.Router();
+const focusTimeController = require('../controllers/focusTimeController');
 
 // Route to save focus time
-router.post('/api/focus-time', saveFocusTime);
+router.post('/', focusTimeController.saveFocusTime);
 
 // Route to get focus times for a specific user
-router.get('/api/focus-time/:userId', getFocusTimes);
+router.get('/:userId', focusTimeController.getFocusTimes);
 
 module.exports = router;
+
