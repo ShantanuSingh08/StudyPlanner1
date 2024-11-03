3b3a4c5d80c563a2e068f088ad37f8db2a2c152b
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 15:38:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('../models/Users');
+const User = require('./models/Users');
 require('dotenv').config();
 
 const app = express();
