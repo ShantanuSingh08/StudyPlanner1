88ace9cf7e8cab842b34978ef9e057ad9406ad2e
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-04 00:19:11 +0530

    Update FocusTime.js

--- models/FocusTime.js
+++ models/FocusTime.js
@@ -1,22 +1,20 @@
-//models/FocusTime.js
-
+// models/FocusTime.js
 const mongoose = require('mongoose');
 
-const FocusTimeSchema = new mongoose.Schema({
+const focusTimeSchema = new mongoose.Schema({
   userId: {
-    type: String,
-    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
   date: {
     type: String,
-    required: true,
+    required: true
   },
   timeInSeconds: {
     type: Number,
-    required: true,
-  },
-}, { timestamps: true });
+    default: 0
+  }
+});
 
-const FocusTime = mongoose.model('FocusTime', FocusTimeSchema);
-
-module.exports = FocusTime;
+module.exports = mongoose.model('FocusTime', focusTimeSchema);
