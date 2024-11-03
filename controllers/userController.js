63e3c40da7aebc9523fc7d15dc1a1c73bebbf079
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-03 17:06:01 +0530

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+// controllers/userController.js
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/Users');
+
+// Registration logic
+const registerUser = async (req, res) => {
+  const { email, password, name, dob, classOrCourse, school } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword, name, dob, classOrCourse, school });
+    await user.save();
+    res.status(201).json({ message: 'Registration successful' });
+  } catch (error) {
+    res.status(400).json({ message: 'Registration failed' });
+  }
+};
+
+// Login logic
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { registerUser, loginUser };
