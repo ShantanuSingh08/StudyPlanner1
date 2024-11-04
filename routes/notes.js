b9822b8b7f6427d41b5c4a83f933a25412af3015
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-04 15:47:47 +0530

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 // routes/notes.js
 const express = require('express');
-const { getNotes, createNote } = require('../controllers/noteController');
+const { getNotes, createNote, deleteNote } = require('../controllers/noteController');
 const router = express.Router();
 
 // GET notes for a specific user
