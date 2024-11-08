3fd573d3c91b0a0424236fa1d96ea30cdd100c4c
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-09 03:38:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use('/api/events', eventRoutes);
 app.use('/api/notes', notesRoutes);
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
