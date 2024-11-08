8840b274c7c668c4de6a5185b31045dec6460e35
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-09 03:00:50 +0530

    Update otpModel.js

--- models/otpModel.js
+++ models/otpModel.js
@@ -1,11 +1,17 @@
-// otpModel.js
 const mongoose = require('mongoose');
 
+// OTP schema definition
 const otpSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  otp: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
+  email: { type: String, required: true },  // Email to which OTP is sent
+  otp: { type: Number, required: true },  // OTP number
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
 });
+
+// TTL index to automatically delete OTP after 5 minutes (300 seconds)
+otpSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 });
 
 const Otp = mongoose.model('Otp', otpSchema);
 
